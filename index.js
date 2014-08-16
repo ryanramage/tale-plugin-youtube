14fec759d103bcb5ea070dbead2fe811bec8cd6b
Ryan Ramage <ryan.ramage@gmail.com>
2014-08-16 10:02:54 -0600

    update to new plugin init

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 module.exports = function(options) {
   if (!options) options = {}
-  return render.bind(null, options);
+  return init.bind(null, options);
 }
 
 
-function render(options, ractive, read, chapter, key, done) {
+function init(options, ractive, read) {
 
   if (!options) options = {};
-  if (!options.ractive_key) options.ractive_key = 'content';
+  if (!options.ractive_key) options.ractive_key = 'youtube';
 
+  ractive.on('chapter', function(chapter, key) {
+    if (!chapter.youtube) return ractive.set('youtube', null);
 
-  if (chapter.youtube) {
     var embedURL= 'http://www.youtube.com/embed/'+ chapter.youtube +'?autoplay=1&controls=0&modestbranding=1'
 
     if (chapter.start_time) embedURL += '&start=' + chapter.start_time;
     if (chapter.end_time)   embedURL += '&end='   + chapter.end_time;
 
-    ractive.set('youtube', embedURL);
-    done();
-  }
+    ractive.set(options.ractive_key, embedURL);
+
+  })
+
 }
 
